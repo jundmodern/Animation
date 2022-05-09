5663d6cbfc54c8b8db06b5ffe5e8f2f70fd4ded2
Seth Singletary <singletaryseth8@gmail.com>
2022-05-09 18:40:20 -0400

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Seth Singletary on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
